a3c8c9f8ee7033659c5d3d193e9f7915f0794292
ArRosid <ahmadrosid30121997@gmail.com>
2019-08-28 20:33:20 +0700

    Setup Serializers for Ebook & Reviews

---
+++ ebooks/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from ebooks import models
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Review
+        fields = "__all__"
+
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Ebook
+        fields = "__all__"
+
